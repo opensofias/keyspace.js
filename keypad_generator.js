ca2851150a7cd592d0a8cd62f9100d1109910765
opensofias <sofias@radikalismus.com>
2017-03-07 19:02:32 +0100

    refactoring and hardly viable product

--- keypad_generator.js
+++ keypad_generator.js
@@ -1,126 +1,102 @@
-
 "use strict";
 
-
-var audio = new AudioContext ();
-var globalGain = audio.createGain ();
-globalGain.connect (audio.destination);
+var audio = new AudioContext ()
+var globalGain = audio.createGain ()
+globalGain.connect (audio.destination)
 
 var settings =
 {
-  controls:
-  {
-    base_freq: document.getElementById("base_freq"),
-    waveform: document.getElementById("waveform"),
-    move_basekey: document.getElementById("move_basekey"),
-    volume: document.getElementById("volume")
-  },
-  
-  refresh: function()
-  {
-    this.base_freq = this.controls.base_freq.valueAsNumber;
-    this.waveform = this.controls.waveform.value;
-    this.move_basekey = this.controls.move_basekey.valueAsNumber;
-    globalGain.gain.value = this.volume = this.controls.volume.valueAsNumber;
-  },
-  
-  init: function () {
-    var refresh = this.refresh.bind(settings);
-    this.controls.base_freq.addEventListener("change", refresh);
-    this.controls.waveform.addEventListener("change", refresh);
-    this.controls.move_basekey.addEventListener("change", refresh);
-    this.controls.volume.addEventListener("change", refresh);
-
+	controls:
+	{
+		base_freq: document.getElementById("base_freq"),
+		waveform: document.getElementById("waveform"),
+		touch: document.getElementById("touch"),
+		volume: document.getElementById("volume")
+	},
 	
-    refresh();
-  }
+	refresh: function ()
+	{
+		this.base_freq = this.controls.base_freq.valueAsNumber
+		this.waveform = this.controls.waveform.value
+		this.touch = this.controls.touch.valueAsNumber
+		globalGain.gain.value = this.volume = this.controls.volume.valueAsNumber
+	},
+	
+	init: function ()
+	{
+		var refresh = this.refresh.bind(settings);
+		this.controls.base_freq.addEventListener("change", refresh);
+		this.controls.waveform.addEventListener("change", refresh);
+		this.controls.touch.addEventListener("change", refresh);
+		this.controls.volume.addEventListener("change", refresh);
+
+		refresh();
+	}
 }
 
 settings.init();
 
 
+class Key
+{
+	constructor (factor)
+	{
+		this.el = document.createElement ("div")
+		this.el.classList.add ("key")
+		factor === 1 && this.el.classList.add ("basekey")
 
-function Key (factor, element) {
-  this.element = element;
-  this.gain = audio.createGain ();
-  this.gain.connect (globalGain);
-  this.factor = factor;
-  
-  var press = this.keyPress.bind(this);
-  var release = this.keyRelease.bind(this);
-  
-  this.element.addEventListener("touchstart", press, false);
-  this.element.addEventListener("mousedown",  press, false);
-  this.element.addEventListener("touchend",    release, false);
-  this.element.addEventListener("touchcancel", release, false);
-  this.element.addEventListener("mouseup",     release, false);
-  this.element.onselectstart = function() {return false; };
-  this.element.oncontextmenu = function(){ return false };
-  
+		this.gain = audio.createGain ().connect (globalGain)
+		this.factor = factor
+
+		var press = this.press.bind(this)
+		var release = this.release.bind(this)
+
+
+
+		this.el.addEventListener("touchstart", press, false)
+		this.el.addEventListener("mousedown", press, false)
+		this.el.addEventListener("touchend", release, false)
+		this.el.addEventListener("touchcancel", release, false)
+		this.el.addEventListener("mouseup", release, false)
+		this.el.addEventListener("mouseout", release, false)
+		this.el.onselectstart = () => false
+		this.el.oncontextmenu = () => false
+	}
+
+	press ()
+	{
+		this.osci = audio.createOscillator ()
+		this.osci.frequency.value = settings.base_freq * this.factor
+		this.osci.connect(this.gain)
+		console.log(settings.waveform)
+		this.osci.type = settings.waveform
+		this.osci.start()
+		this.el.classList.add ("pressed")
+	}
+
+	release ()
+	{
+		this.osci.stop(); this.osci.disconnect()
+		this.el.classList.remove ("pressed")
+	}
 }
 
-Key.prototype = {
-  keyPress: function ()
-  {
-    this.osci = audio.createOscillator ();
-    this.osci.frequency.value = settings.base_freq * this.factor;
-    this.osci.connect(this.gain);
-    console.log(settings.waveform);
-    this.osci.type = settings.waveform;
-    this.osci.start();
-  },
-  
-  keyRelease: function ()
-  {
-    this.osci.stop();
-    this.osci.disconnect();
-  }
+class KeyPad
+{
+	constructor (interval, fraction, keys)
+	{
+		this.el = document.createElement ("div")
+		let count = 0
+		do
+		{
+			const factor = Math.pow (interval, count / fraction)
+			const key = new Key (factor)
+			key.el.style.left = (count * 64) + "px"
+			this.el.appendChild (key.el)
+		}
+		while (count ++ < keys)
+
+	}
 }
 
-var basekey = new Key (1, document.getElementById("basekey"));
-var blakey = new Key (2, document.getElementById("blakey"));
-
-/*function drag_start(event) {
-    var style = window.getComputedStyle(event.target, null);
-    event.dataTransfer.setData("text/plain",
-    (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
-}
-function drag_over(event) {
-    event.preventDefault();
-    return false;
-}
-
-function drop(event) {
-    var offset = event.dataTransfer.getData("text/plain").split(',');
-    basekey.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
-    basekey.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
-    event.preventDefault();
-    return false;
-}
-basekey.addEventListener('dragstart',drag_start,false);
-document.body.addEventListener('dragover',drag_over,false);
-document.body.addEventListener('drop',drop,false);
-*/
-
-
-function drag_start(event) {
-    var style = window.getComputedStyle(event.target, null);
-    event.dataTransfer.setData("text/plain",
-    (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
-}
-function drag_over(event) {
-    event.preventDefault();
-    return false;
-}
-
-function drop(event) {
-    var offset = event.dataTransfer.getData("text/plain").split(',');
-    basekey.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
-    basekey.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
-    event.preventDefault();
-    return false;
-}
-
-basekey.addEventListener('dragstart',drag_start,false);
-document.body.addEventListener('dragover',drag_over,false);
-document.body.addEventListener('drop',drop,false);
+document.body.appendChild (new KeyPad (2,16,16).el)
