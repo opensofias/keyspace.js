20e18959024f80436402c1f17fab6864b0f1635b
sofia <12581975+opensofias@users.noreply.github.com>
2023-09-04 00:18:10 +0200

    using pointer events instead of touch and mouse

--- keypad_generator.js
+++ keypad_generator.js
@@ -37,8 +37,6 @@ var settings =
 
 settings.init();
 
-var touch = false
-
 class Key
 {
 	constructor (factor)
@@ -52,22 +50,11 @@ class Key
 
 		var press = this.press.bind(this)
 		var release = this.release.bind(this)
-
-
-
-
-		if (touch)
-		{
-			this.el.addEventListener("touchstart", press, false)
-			this.el.addEventListener("touchend", release, false)
-			this.el.addEventListener("touchcancel", release, false)
-		}
-		else
-		{
-			this.el.addEventListener("mousedown", press, false)
-			this.el.addEventListener("mouseup", release, false)
-			this.el.addEventListener("mouseout", release, false)
-		}
+		
+		this.el.addEventListener("pointerdown", press, false)
+		this.el.addEventListener("pointerup", release, false)
+		this.el.addEventListener("pointerout", release, false)
+		
 		this.el.onselectstart = () => false
 		this.el.oncontextmenu = () => false
 	}
