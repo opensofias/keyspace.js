03aa8f4086b5ae511c4a8e177604d8e87861e336
sofia <12581975+opensofias@users.noreply.github.com>
2023-09-03 23:31:39 +0200

    fixing some errors in the unused keypads.js

--- keypads.js
+++ keypads.js
@@ -1,13 +1,13 @@
 var classical =
 {
 	dims : 2,
-	min : [-1, -1]
+	min : [-1, -1],
 	max : [-1, -1],
 	freq : function (d1, d2) {return base.freq * Math.exp(2, d1 / 12 + d2);},
 	pos_x : function (d1, d2) {return base.pos_x + 32 * d1;},
 	pos_y : function (d1, d2) {return base.pos_y + 128 * d2;},
-	size_x: 32;
-	size_y: 128;
+	size_x: 32,
+	size_y: 128,
 }
 
 var harmonic12 =
